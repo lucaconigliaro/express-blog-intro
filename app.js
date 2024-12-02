ebf63709ed0e231af7ae9f63697ba45c41aced21
lucaconigliaro <lucaconigliaro1@gmail.com>
2024-12-02 14:40:06 +0100

    Asset statici configurati e testato il tutto su postman

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const posts = require("./data"); // Importiamo il file `data.js` che contiene i 
 const app = express(); // Creiamo un'applicazione Express.
 const port = 3001; // Definiamo la porta su cui il server ascolterà (3001).
 
+//Configuriamo gli asset statici sull’applicazione in modo che si possano visualizzare le immagini associate ad ogni post.
+app.use(express.static(`public`));
+
 // Creiamo una rotta per la homepage (`/`) che risponde con un messaggio JSON.
 app.get(`/`, (req, res) => {
     res.json({
@@ -13,7 +16,7 @@ app.get(`/`, (req, res) => {
 // Creiamo poi una rotta `/bacheca` che restituisca un oggetto json con la lista dei post e il conteggio, partendo da un array.
 app.get(`/bacheca`, (req, res) => {
     const result = {
-        dolci: posts, 
+        dolci: posts,  
         totale: posts.length
     }
     res.json(result);
--- data.js
+++ data.js
@@ -2,31 +2,31 @@ const posts = [
     {
         titolo: "Ciambellone soffice",
         contenuto: "Un dolce soffice e profumato, perfetto per la colazione o la merenda.",
-        immagine: ".images/ciamebllone.jpg",
+        immagine: "/images/ciambellone.jpeg",
         tags: ["dolce", "ciambella", "soffice", "colazione"]
     },
     {
         titolo: "Cracker di barbabietole",
         contenuto: "Uno snack croccante e colorato, ideale per aperitivi o spuntini leggeri.",
-        immagine: ".images/cracker_barbabietola.jpg",
+        immagine: "/images/cracker_barbabietola.jpeg",
         tags: ["salato", "barbabietole", "snack", "croccante"]
     },
     {
         titolo: "Pane fritto dolce",
         contenuto: "Un delizioso pane fritto, arricchito con zucchero e cannella per un tocco dolce.",
-        immagine: ".images/pane_fritto_dolce.jpg",
+        immagine: "/images/pane_fritto_dolce.jpeg",
         tags: ["dolce", "pane", "fritto", "tradizionale"]
     },
     {
         titolo: "Pasta alle barbabietole",
         contenuto: "Un piatto originale e colorato, preparato con pasta fatta in casa e barbabietole.",
-        immagine: ".images/pasta_barbabietola.jpg",
+        immagine: "/images/pasta_barbabietola.jpeg",
         tags: ["barbabietole", "colorato", "pasta", "gourmet"]
     },
     {
         titolo: "Torta paesana",
         contenuto: "Un dolce rustico e tradizionale, a base di pane raffermo, cacao e uvetta.",
-        immagine: ".images/torta_paesana.jpg",
+        immagine: "/images/torta_paesana.jpeg",
         tags: ["tradizionale", "rustico", "cioccolato", "dessert"]
     }
 ];
